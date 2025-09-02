89894e8f895a943a4f2e511ccad5cd9c0886d77e
vitor pallis <vitorpallis@gmail.com>
2025-09-01 22:16:32 -0300

    FEAT: adicionei o html, css ,e o js

---
+++ src/js/app.js
@@ -0,0 +1,52 @@
+let jogadoras = [
+  {
+    "nome": "Andressa Alves",
+    "posicao": "Meio-campo",
+    "clube": "Corinthians",
+    "foto": "https://example.com/andressa.jpg",
+    "gols": 15,
+    "assistencias": 10,
+    "jogos": 28,
+    "favorita": false
+  },
+  {
+    "nome": "Dayana Rodríguez",
+    "posicao": "Meio-campo",
+    "clube": "Corinthians",
+    "foto": "https://example.com/dayana.jpg",
+    "gols": 5,
+    "assistencias": 12,
+    "jogos": 30,
+    "favorita": false
+  },
+  {
+    "nome": "Mariza",
+    "posicao": "Zagueira",
+    "clube": "Corinthians",
+    "foto": "https://example.com/mariza.jpg",
+    "gols": 2,
+    "assistencias": 1,
+    "jogos": 32,
+    "favorita": false
+  },
+  {
+    "nome": "Thaís Regina",
+    "posicao": "Zagueira",
+    "clube": "Corinthians",
+    "foto": "https://example.com/thais.jpg",
+    "gols": 1,
+    "assistencias": 2,
+    "jogos": 25,
+    "favorita": false
+  },
+  {
+    "nome": "Letícia Teles",
+    "posicao": "Zagueira",
+    "clube": "Corinthians",
+    "foto": "https://example.com/leticia.jpg",
+    "gols": 0,
+    "assistencias": 0,
+    "jogos": 18,
+    "favorita": false
+  }
+];
