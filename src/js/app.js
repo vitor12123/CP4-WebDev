1c73ec48434c759a33c09596e413b7405c0865b8
vitor pallis <vitorpallis@gmail.com>
2025-09-05 23:39:22 -0300

    ADD, BUGFIX: botao de deletar, botao de editar(incompleto), e recurso de favoritar

--- src/js/app.js
+++ src/js/app.js
@@ -51,9 +51,17 @@ let jogadoras = [
   }
 ];
 
+let contador = 0;
+let ativado = false;
+let jogadorasFavoritas = []
+
 window.onload = () => {
   exibir();
 }
+
+function idx(event) {
+    const idx = event.target.dataset.index;
+};
 
 function exibir() {
   const localDasJogadoras = document.getElementById('sectionJogadoras');
@@ -61,17 +69,59 @@ function exibir() {
     const card = document.createElement('div')
     card.className ='listaJogadoras' 
     card.innerHTML += `
+        <img id="favorito" src="../img/favorite_24dp_000_FILL0_wght400_GRAD0_opsz24.png" alt="">
         <img class="card" src="../img/cardJogadoras.png" alt="">
         <div class='informacaoJogadoras'>
+        <img class="fotoJogadora" src="${procurar.foto}" alt="${procurar.foto}">
         <h2 class="nomeJogadora">${procurar.nome}</h2>
         <p>Posição: ${procurar.posicao}</p>
         <p>Clube: ${procurar.clube}</p>
-        <img class="fotoJogadora" src="${procurar.foto}" alt="${procurar.foto}">
         <p>Gols: ${procurar.gols}</p>
         <p>Assistências: ${procurar.assistencias}</p>
         <p>Jogos: ${procurar.jogos}</p>
         </div>
+        <button class="botaoDelete">Excluir Card da jogadora</button>
+        <button class="botaoEditar" data-index="${idx}">Editar Card da jogadora</button>
     `
-    localDasJogadoras.appendChild(card); 
+    localDasJogadoras.appendChild(card);
+    let foto = card.append(procurar.foto)
+    
+    let favorito = card.querySelector('#favorito');
+    favorito.addEventListener('click', () => {
+      ativado =  !ativado;
+      if (ativado == true ) {
+        favorito.style.backgroundColor = 'red';
+        favorito.style.borderRadius = '60%';
+        jogadorasFavoritas.push(procurar.nome)
+        localStorage.setItem("JogagadorasFavoritadas", jogadorasFavoritas)
+      }
+      else {
+        favorito.style.backgroundColor = "#ffff";
+        jogadorasFavoritas = jogadorasFavoritas.filter(nm => nm !== procurar.nome)
+        localStorage.removeItem("JogadorasFavoritas", jogadorasFavoritas)
+      }
+      contador++; 
+    });
+
+  let edit = card.querySelectorAll('.botaoEditar');
+  edit.forEach(btnEdit => {
+    btnEdit.addEventListener("click", (event) => {
+      const novaImg = prompt('digite uma nova URL para mudara foto, se nao for mudar coloque a mesma URL.', jogadoras[idx], foto)
+      jogadoras[idx].foto = novaImg
+      localDasJogadoras.innerHTML = "";
+      exibir();
+      });
+    });
+  
+  let delet = card.querySelectorAll('.botaoDelete');
+  delet.forEach(btnDel => {
+    btnDel.addEventListener("click", () => {
+      jogadoras = jogadoras.filter(x => x.nome !== procurar.nome);
+      const localDasJogadoras = document.getElementById('sectionJogadoras'); 
+      localDasJogadoras.innerHTML = "";
+      exibir();
+      });
+    });
   });
+
 }
